daa6b32a101d9ebb3b0219f64de514c665d13fbf
roaddust2 <roaddust2@yahoo.com>
2023-01-10 13:16:03 +0400

    Fix linter errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -26,8 +26,7 @@ logging.basicConfig(
     filename='logs.log',
     filemode='w',
     format='%(asctime)s %(levelname)s:%(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-    )
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 logger = logging.getLogger(__name__)
 
